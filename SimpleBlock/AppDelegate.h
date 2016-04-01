77a491f56ad02046f24c3fb3e3f74816d8baa69a
YISHANG <minwoowei@yeha.net>
2016-04-01 09:53:35 +0800

    Initial Commit

---
+++ SimpleBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleBlock
+//
+//  Created by YISHANG on 16/4/1.
+//  Copyright © 2016年 YISHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleBlock
+//
+//  Created by YISHANG on 16/4/1.
+//  Copyright © 2016年 YISHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
