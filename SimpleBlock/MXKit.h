fa9d32fd316c91256283ea2ea2d19cb219e3eee0
MAXIAO <845423794@qq.com>
2016-04-13 18:57:38 +0800

    MXKit蓝图

---
+++ SimpleBlock/MXKit.h
@@ -0,0 +1,18 @@
+//
+//  MXKit.h
+//  SimpleBlock
+//
+//  Created by YISHANG on 16/4/13.
+//  Copyright © 2016年 YISHANG. All rights reserved.
+//
+
+// MXKit 跨向架构师的第一步
+// 分层管理框架, 各个功能层之间不能有太多依赖
+// 网络层
+// 数据层 (FMDB) SQL
+// Cache层
+// UI层
+
+// 一些设置 Until Config全局设置等
+// 待完善 架构设计需要对api接口设计, 函数方法的抽取, 连式编程思路
+// 低耦合 , 高内聚
