d6d53df0cc854491efc99fda654515f8b43879da
mhqamx <845423794@qq.com>
2016-04-26 20:03:06 +0800

    commit

--- SimpleBlock/AppDelegate.m
+++ SimpleBlock/AppDelegate.m
@@ -11,12 +11,10 @@
 @interface AppDelegate ()
 
 @end
-
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
